5d815a049628f23ce877358c186fe695d955aa52
OI61 <35666112+OI61@users.noreply.github.com>
2024-07-03 21:08:34 -0300

    Add files via upload

---
+++ TmR/Termos.php
@@ -0,0 +1,83 @@
+<?php include 'php/header.php';?>
+<link rel="stylesheet" href="termos.css"/>
+
+<section class="parte1">
+	<div class="text">
+		<h1>Termos e Responsabilidade Da Arcade</h1>
+	</div>
+</section>
+
+
+<section class="parte2">
+	<div class="text2">
+		<h1>Política de Privacidade de Clientes da Arcade<br>
+A Arcade tem o compromisso de proteger a sua privacidade.<p>
+  Suas informações são armazenadas em nossos servidores e são criptografados de ponta a ponta.
+</p></h1>
+	</div>
+</section>
+
+<section class="parte3">
+	<div class="text3">
+		<h1>Política de Privacidade da Arcade
+Atualizado em 16 de outubro de 2022
+A Política de Privacidade da Arcade descreve como a Arcade coleta, usa e compartilha seus dados pessoais.
+
+Além desta Política de Privacidade, nossos produtos e determinados recursos que solicitam o uso das suas informações pessoais também incluem informações sobre dados e privacidade. Essas informações específicas do produto são acompanhadas por nosso ícone de privacidade e dados. 
+
+
+
+Você terá a oportunidade de analisar as informações específicas sobre o produto antes de usar os recursos. Você também pode ver essas informações a qualquer momento, nas configurações relacionadas a esses recursos.
+
+Acesse os links abaixo e dedique um momento para se familiarizar com nossas práticas de privacidade. Em caso de dúvidas, entre em contato conosco (suporte).</h1>
+
+	</div>
+</section>
+
+<section class="parte4">
+	<div class="text4">
+		<h1>Politia de Responsabilidade</h1>
+		<h2><br>
+	
+</h2>
+<ul>
+	<li><h3>Analogia a crimes;</h3></li>
+	<li><h3>Sexualização ou exploração sexual de menores, como aliciamento, comentários de cunho sexual ou imagens inadequadas. Se encontrarmos esse tipo de conteúdo, ele será denunciado às autoridades competentes;</h3></li>
+	<li><h3>uso de cheat;</h3></li>
+	<li><h3>uso indevido de conta;</h3></li>
+	<li><h3>Promover, vender ou negociar produtos ilegais;</h3></li>
+	<li><h3>Zombaria ou ataque a crenças, símbolos sagrados, movimentos ou instituições;</h3></li>
+	<li><h3>Conteúdo falso ou enganoso sobre indivíduos ou grupos protegidos que dissemine medo, ódio ou preconceito.</h3></li>
+</ul>
+	
+	</div>
+</section>
+
+
+<section class="parte5">
+	<div class="text5">
+		<h1>Arcade</h1>
+
+		<h2>A Arcade não é um lugar para conteúdo ou comportamento antagônico, explícito, falso ou enganoso, nocivo, odioso ou violento. Podemos remover, limitar ou bloquear a distribuição de tal conteúdo e as contas, indivíduos, grupos e domínios que o criam ou divulgam com base na quantidade de dano que ele representa.
+
+</h2>
+</section>
+
+<?php include 'php/footer.php';?>
+
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+    <!-- //////////////// BACK TO TOP BUTTON //////////////// -->
+    <a href="#" class="to-top-btn">
+        <i class='bx bx-chevron-up'></i>
+    </a>
+
+
+</body>
+</html>
