8bc8237d023b418d291288294509398a5f506181
OI61 <35666112+OI61@users.noreply.github.com>
2024-07-03 21:00:44 -0300

    Add files via upload

---
+++ categoriaAnimes/DemonSlayer.php
@@ -0,0 +1,61 @@
+<?php include 'php/header.php';?>
+<link rel="stylesheet" href="../css/paginacompra.css"/>
+
+ <!-- //////////////// Description section ///////////////-->
+ <section>
+    	<div class="desc">
+    		<h1>Demon Slayer: Kimetsu no Yaiba The <br>Hinokami Chronicles</h1>
+    		<div class="text"><h1>R$299,99</h1></div>
+    		<button>Ir para a Simulação</button>
+    		<br>
+    		<br>
+    		<br>    		
+    		<button>Adicionar ao Carrinho</button>
+    		<img src="images/logo_demon.webp">
+    	</div>
+    	    </section>
+
+<!-- //////////////// TOP BOX //////////////// -->
+    <section>
+     
+      </div>
+      <div class="ret"><p>Historia do Jogo</p></div>
+       <!-- //////////////// ABOUT SECTION //////////////// -->
+    <section>
+      <div class="about-section">
+          <div class="dentro"><p style="font-size: 25px;">Demon Slayer conta a história de Tanjiro Kamado, um rapaz que vê sua vida mudar quando sua família é assassinada por um demônio. Para piorar, sua irmã Nezuko, a única sobrevivente do massacre, é transformada em um desses seres demoníacos.</p>
+            <br>
+            <br>
+            <p style="font-size: 20px;">Para vingar a sua família e encontrar uma cura para a irmã, Tanjiro se torna um Caçador de Demônios, utilizando uma espada especial capaz de acabar de vez com as criaturas malignas. Em suas aventuras, o rapaz é acompanhado por Zenitsu e Inosuke, que juntos enfrentam inúmeros desafios e oponentes perigosos.</p></div>
+        
+          
+        </div>
+        <div class="about-content">
+      
+        </div>
+      </div>
+    </section>
+    </section>
+    
+
+    <!-- //////////////// Caracteriscas SECTION //////////// -->
+    <section>
+    	<div class="carac">
+    		<h1>Características</h1>
+    	</div>
+    	<div class="cimg">
+    		<img src="images/demon1.webp">
+    		<img src="images/demon2.webp">
+    		<img src="images/demon3.webp">
+    		<img src="images/demon4.webp">
+    	</div>
+    </section>
+
+    <!-- //////////////// ABOUT SECTION //////////////// -->
+
+    <!-- //////////////// SOCIAL MEDIA SECTION //////////////// -->
+    <section>
+      
+    </section>
+
+<?php include 'php/footer.php';?>
---
+++ categoriaAnimes/granblue.php
@@ -0,0 +1,61 @@
+<?php include 'php/header.php';?>
+<link rel="stylesheet" href="../css/paginacompra.css"/>
+
+ <!-- //////////////// Description section ///////////////-->
+ <section>
+    	<div class="desc">
+    		<h1>Granblue Fantasy: Versus</h1>
+    		<div class="text"><h1>R$39,99</h1></div>
+    		<button>Ir para a Simulação</button>
+    		<br>
+    		<br>
+    		<br>    		
+    		<button>Adicionar ao Carrinho</button>
+    		<img src="images/logo_granblue.webp">
+    	</div>
+    	    </section>
+
+<!-- //////////////// TOP BOX //////////////// -->
+    <section>
+     
+      </div>
+      <div class="ret"><p>Historia do Jogo</p></div>
+       <!-- //////////////// ABOUT SECTION //////////////// -->
+    <section>
+      <div class="about-section">
+          <div class="dentro"><p style="font-size: 25px;">No desenrolar disso, descobrimos que alguém está usando uma criatura para modificar as memórias dos personagens e incentivar que eles atuem da forma mais propicia para que seu plano se realize. Sobrando para Gran e seus amigos resolveram os diversos problemas que estão ocorrendo, ajudando quem está sendo controlado e salvando, assim, o mundo.</p>
+            <br>
+            <br>
+            <p style="font-size: 20px;">O especial da história é a forma que ela é entregue. Devemos fazer quests num estilo “beat’em up” que se resumem em derrotar um número de personagens, sobreviver por um tempo determinado ou confrontar um chefão. A primeira vista parece algo repetitivo, contudo, a diversidade dos inimigos faz com que sempre tenha algo “novo” para enfrentar. Além disso, esse modo simula como seria o mobile game com uma jogabilidade mais dinâmica, sendo claro que os fãs já existentes são o público alvo de Granblue Fantasy Versus.</p></div>
+        
+          
+        </div>
+        <div class="about-content">
+      
+        </div>
+      </div>
+    </section>
+    </section>
+    
+
+    <!-- //////////////// Caracteriscas SECTION //////////// -->
+    <section>
+    	<div class="carac">
+    		<h1>Características</h1>
+    	</div>
+    	<div class="cimg">
+    		<img src="images/granblue1.webp">
+    		<img src="images/granblue2.jpg">
+    		<img src="images/granblue3.jpg">
+    		<img src="images/granblue4.jpg">
+    	</div>
+    </section>
+
+    <!-- //////////////// ABOUT SECTION //////////////// -->
+
+    <!-- //////////////// SOCIAL MEDIA SECTION //////////////// -->
+    <section>
+      
+    </section>
+
+<?php include 'php/footer.php';?>
