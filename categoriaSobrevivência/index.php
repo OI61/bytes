7241d59333ad40a96858128bc6561d2937f10e3e
OI61 <35666112+OI61@users.noreply.github.com>
2024-07-03 21:02:39 -0300

    Add files via upload

---
+++ categoriaSobrevivência/index.php
@@ -0,0 +1,161 @@
+<?php include 'php/header.php';?>
+<link rel="stylesheet" href="categoria.css"/>
+
+<!-- //////////////// BODY SECTION //////////////// -->
+<div class="categoria">
+  <h1>Categorias</h1>
+  <img src="images/a1.png">
+</div>
+<div class="video">
+  <div class="centro">
+    <h1><b>Sobrevivência</b></h1>
+  </div>
+  <video autoplay loop  muted>
+    <source src="video/mine.mp4" type="video/mp4 ">
+  </video>
+</div>
+<div class="Principais">
+  <h1><b>PRINCIPAIS</b></h1>
+</div>
+
+<!-- //////////////// PRINCIPAIS //////////////// -->
+<section class="container">
+    <div class="box">
+      <img src="images/astro.jpg" height="100%">
+    </div>
+    <div class="box">
+      <img src="images/subnautica.jpg" height="100%">
+    </div>
+    <div class="box">
+      <img src="images/grounded.jpg" height="100%">
+    </div>
+    <div class="box">
+      <img src="images/ark.webp" height="100%">
+    </div>
+    <div class="box">
+      <img src="images/mine.webp" height="100%">
+    </div>
+  </section>
+</div>
+
+<!-- //////////////// OUTROS //////////////// -->
+</section>
+
+<div class="Outros"><h1><b>OUTROS</b></h1></div>
+
+<section>
+      <div class="cards-section">
+     
+            <div class="cards-box">
+          <div class="card-item">
+            <h3>Minecraft</h3>
+            <img src="images/minelogo.webp" height="80px">
+            <p></p>
+            <span>R$ 129,90</span>
+            <button>Descubra  +</button>
+          </div>
+
+        
+      </div>
+
+          <div class="cards-box">
+          <div class="card-item">
+            <h3>ARK</h3>
+            <img src="images/arklogo.png" height="100px">
+            <p></p>
+            <span>R$ 49,95</span>
+            <button>Descubra  +</button>
+          </div>
+
+        
+      </div>
+
+
+          <div class="cards-box">
+          <div class="card-item" height="70px">
+            <h3>Dayz</h3>
+            <img src="images/dayzlogo.png" height="80px">
+            <p></p>
+            <span>R$ 119,90</span>
+            <button>Descubra  +</button>
+          </div>
+ 
+
+
+      </div>
+       <div class="cards-box">
+          <div class="card-item">
+            <h3>Dead by daylight</h3>
+            <img src="images/dbdlogo.png"  height="90px">
+            <p></p>
+            <span>R$ 49,90</span>
+            <button>Descubra  +</button>
+          </div>
+ 
+
+
+      </div>
+  </div>
+</section>
+ <section>
+      <div class="cards-section">
+     
+            <div class="cards-box">
+          <div class="card-item">
+            <h3>Dying light 2</h3>
+            <img src="images/dl2logo.png" height="55px">
+            <p></p>
+            <span>R$ 309,76</span>
+            <button>Descubra  +</button>
+          </div>
+
+        
+      </div>
+
+
+          <div class="cards-boxre">
+          <div class="card-item">
+            <h3>Resident Evil</h3>
+            <img src="images/relogo.webp" height="150px">
+            <p></p>
+            <span>Verificar cronologia</span>
+            <button>Descubra  +</button>
+          </div>
+
+        
+      </div>
+
+
+          <div class="cards-box">
+          <div class="card-item">
+            <h3>Escape From Tarkov</h3>
+            <img src="images/tarkovlogo.png" height="100px">
+            <p></p>
+            <span>R$ 232,24</span>
+            <button>Descubra  +</button>
+          </div>
+ 
+
+
+      </div>
+       <div class="cards-box">
+          <div class="card-item">
+            <h3>Terraria</h3>
+            <img src="images/terrarialogo.png" height="70px">
+            <p></p>
+            <span>R$ 19,99</span>
+            <button>Descubra  +</button>
+          </div>
+ 
+
+
+      </div>
+  </div>
+
+
+
+
+
+
+
+<?php include 'php/footer.php';?>
---
+++ categoriaSobrevivência/php/footer.php
@@ -0,0 +1,74 @@
+<footer class="footer">
+      <div class="container footer-wrapper">
+        <div class="logo-col">
+          <a href="#" class="footer-logo1">
+            <img src="images/logo.png" alt="Company Logo" class="logo1" />
+          </a>
+
+        
+          </ul>
+
+          <p class="copyright">
+            Copyright &copy; bytes inc
+          </p>
+        </div>
+
+        <div class="address-col">
+          <p class="footer-heading"></p>
+          <address class="contacts">
+            <p class="address"></p>
+            <p>
+              <a href="tel:206-9414-5674" class="footer-link"></a>
+              <br />
+              <a href="mailto:info@company.com" class="footer-link"
+                ></a
+              >
+            </p>
+          </address>
+        </div>
+
+        <nav class="nav-col">
+       
+          <ul class="footer-nav">
+            <li><a href="../sobre/sobre.html" class="footer-link">Sobre Nós</a></li>
+             <li><a href="../TmR/termos.html" class="footer-link">Termos e Serviços</a></li>
+           </li>
+          </ul>
+        </nav>
+
+        <nav class="nav-col">
+         
+          <ul class="footer-nav">
+            
+            <li><a href="../Suporte/index.php" class="footer-link">Suporte</a></li>
+            <li><a href="../descubra.php" class="footer-link">Categorias</a></li>
+            
+        </nav>
+
+        <nav class="nav-col">
+          <p class="footer-heading"></p>
+          <ul class="footer-nav">
+            <li><a href="#" class="footer-link"></a></li>
+            <li><a href="#" class="footer-link"></a></li>
+            <li><a href="#" class="footer-link"></a></li>
+            
+          </ul>
+        </nav>
+      </div>
+    </footer>
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+    <!-- //////////////// BACK TO TOP BUTTON //////////////// -->
+    <a href="#" class="to-top-btn">
+        <i class='bx bx-chevron-up'></i>
+    </a>
+
+
+
+  </body>
