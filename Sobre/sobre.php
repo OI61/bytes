e3aa2bbd000a9f0edf64d895d9682a0d4ed417f1
OI61 <35666112+OI61@users.noreply.github.com>
2024-07-03 21:08:02 -0300

    Add files via upload

---
+++ Sobre/sobre.php
@@ -0,0 +1,76 @@
+<?php include 'php/header.php';?>
+<link rel="stylesheet" href="sobre.css"/>
+<section class="parte1">
+	<div class="text">
+		<h1>Sobre nós</h1>
+	</div>
+</section>
+
+
+<section class="parte2">
+	<div class="text2">
+		<h1>Desde 23 de Julho de 2022</h1>
+	</div>
+</section>
+
+<section class="parte3">
+	<div class="text3">
+		<h1>Colaboradores</h1>
+
+		<ul>
+<li><h3>Bryan Eraldo Joanico</h3></li>
+<li><h3>Caio Guilherme da Silva Rosas</h3></li>
+<li><h3>Carlos Eduardo Peres Gouveia</h3></li>
+<li><h3>Flávio Borges</h3></li>
+<li><h3>Isabel Florão dos Santos</h3></li>
+<li><h3>Pedro Augusto da Silva Cardoso</h3></li>
+<li><h3>Ricardo Alexandre de Araújo Filho</h3></li>
+
+		</ul>
+	</div>
+</section>
+
+<section class="parte4">
+	<div class="text4">
+		<h1>Tempo Trabalhado</h1>
+		<h2>De acordo com o nosso ponto eletrônico tivemos a  estimativa , de 3 meses 4 horas 56 minutos e 49 segundos<br>
+		bem durante o período de criação da faze beta da Arcade , os 7 (sete) integrantes da Arcade  tiveram suas funções dividias em:
+</h2>
+<ul>
+	<li><h3>Bryan Eraldo Joanico- Programação</h3></li>
+	<li><h3>Caio Guilherme da Silva Rosas -Programação DEV</h3></li>
+	<li><h3>Carlos Eduardo Peres Gouveia -Programação DEV & Gestor de Desing de UI</h3></li>
+	<li><h3>Flávio Borges- Divisão GAMES BYTES</h3></li>
+	<li><h3>Isabel Florão dos Santos-Designer</h3></li>
+	<li><h3>Pedro Augusto da Silva Cardoso-Divisão GAMES BYTES</h3></li>
+	<li><h3>Ricardo Alexandre de Araújo Filho-Programação</h3></li>
+</ul>
+	
+	</div>
+</section>
+
+
+<section class="parte5">
+	<div class="text5">
+		<h1>Compromisso</h1>
+
+		<h2>Sempre fazer o melhor para nossos usúarios, trazer o bem estar e a alegria a todos com nossos jogos e jogos de terceiros com um bom valor de compra , e sempre estar ao seu lado.Nós somos a Arcade , uma empresa do Grupo Bytes Computer.</h2>
+</section>
+
+<?php include 'php/footer.php';?>
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+
+    <!-- //////////////// BACK TO TOP BUTTON //////////////// -->
+    <a href="#" class="to-top-btn">
+        <i class='bx bx-chevron-up'></i>
+    </a>
+
+</body>
+</html>
